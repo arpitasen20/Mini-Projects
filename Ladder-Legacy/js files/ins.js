4134acdb6b0ac2350b75daa375c5f53c284c8fe2
Arpita Sen <114492179+arpitasen20@users.noreply.github.com>
2023-12-29 11:50:07 +0530

    .

--- Ladder-Legacy/js files/ins.js
+++
@@ -1,8 +0,0 @@
-const backgroundSound = new Audio("./Audio files/gamemusic-6082.mp3")
-backgroundSound.play()
-backgroundSound.loop = true;
-backgroundSound.volume = 0.5
-
-document.getElementById("instruct-back").addEventListener("click" ,  ()=>{
-    window.location.href = "index.html"
-})
